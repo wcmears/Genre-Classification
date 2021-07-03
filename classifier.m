c070048683975302b5fb8cbf133ffdf71033f8ce
William Campbell Mears III <82682901+wcmears@users.noreply.github.com>
2021-07-03 15:54:55 -0400

    Update classifier.m

--- classifier.m
+++ classifier.m
@@ -1,12 +1,13 @@
 function classifier(inputSignal)
-    signal = audioread(inputSignal);
-    genreMatrix = readmatrix('genreMatrix.txt');
-    classicalrolloffMean = mean(mirgetdata(mirrolloff(miraudio(signal),'frame')));
-    classicalrolloffStd = std(mirgetdata(mirrolloff(miraudio(signal),'frame')));
-    inputVector = [ mean(nonzeros(mean(mirgetdata(mirtonalcentroid(miraudio(signal),'frame'))))) std(nonzeros(std(mirgetdata(mirtonalcentroid(miraudio(signal),'frame'))))) classicalrolloffMean classicalrolloffStd mean(nonzeros(mirgetdata(mirflux(miraudio(signal),'frame')))) std(nonzeros(mirgetdata(mirflux(miraudio(signal),'frame')))) mean(nonzeros(mirgetdata(mirzerocross(miraudio(signal),'frame')))) std(nonzeros(mirgetdata(mirzerocross(miraudio(signal),'frame')))) mean(nonzeros(mirgetdata(mirlowenergy(miraudio(signal))))) ];
+    signal = audioread(inputSignal); #read in input signal
+    genreMatrix = readmatrix('genreMatrix.txt'); #read previously created definition of genres
+    classicalrolloffMean = mean(mirgetdata(mirrolloff(miraudio(signal),'frame'))); #calculate mean rolloff of signal
+    classicalrolloffStd = std(mirgetdata(mirrolloff(miraudio(signal),'frame'))); #calculate std rolloff of signal
+    inputVector = [ mean(nonzeros(mean(mirgetdata(mirtonalcentroid(miraudio(signal),'frame'))))) std(nonzeros(std(mirgetdata(mirtonalcentroid(miraudio(signal),'frame'))))) classicalrolloffMean classicalrolloffStd mean(nonzeros(mirgetdata(mirflux(miraudio(signal),'frame')))) std(nonzeros(mirgetdata(mirflux(miraudio(signal),'frame')))) mean(nonzeros(mirgetdata(mirzerocross(miraudio(signal),'frame')))) std(nonzeros(mirgetdata(mirzerocross(miraudio(signal),'frame')))) mean(nonzeros(mirgetdata(mirlowenergy(miraudio(signal))))) ]; #create row of features for input signal to be compared to genre matrix rows
     for i = 1 : 1 : 4
-        R(i) =  mean(mean(corrcoef(inputVector, genreMatrix(i,:))));
+        R(i) =  mean(mean(corrcoef(inputVector, genreMatrix(i,:)))); #calculate correlation coefficient between data from input source and defined matrix
     end
+    #display genre with highest corrcoef
     if R(1) == max(R)
         disp("rock");
     end

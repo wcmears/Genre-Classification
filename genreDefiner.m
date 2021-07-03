a7a542f57e37b44831d8d2101b6eb0d80c9ed491
William Campbell Mears III <82682901+wcmears@users.noreply.github.com>
2021-07-03 17:16:44 -0400

    Update genreDefiner.m

--- genreDefiner.m
+++ genreDefiner.m
@@ -1,4 +1,6 @@
 function genreDefiner
+  %Input source directories and define as cell, each directory contains songs that user defines to be the same genre
+  %---------------------------------------------------------------------------------------------------------------------
   BasePath = dir('C:\Users\wmears\Documents\MATLAB\Genre Classification\MIRToolbox\SixGenres\genres_original\classical');
   classicalArchives = {BasePath.name};
   BasePath = dir('C:\Users\wmears\Documents\MATLAB\Genre Classification\MIRToolbox\SixGenres\genres_original\blues');
@@ -19,80 +21,82 @@ function genreDefiner
   reggaeArchives = {BasePath.name};
   BasePath = dir('C:\Users\wmears\Documents\MATLAB\Genre Classification\MIRToolbox\SixGenres\genres_original\rock');
   rockArchives = {BasePath.name};
-    for i = 3 : 1 : 10
+  %iterate through directories of defined
+  %----------------------------------------------------------------------------------------------------------------
+    for i = 3 : 1 : len(metalArchives)
       metalCentroidMean(1,i) = mean(mean(mirgetdata(mirtonalcentroid(miraudio(metalArchives(i)),'frame'))));
       metalCentroidStd(1,i) = std(std(mirgetdata(mirtonalcentroid(miraudio(metalArchives(i)),'frame'))));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(metalArchives)
       metalrolloffMean(1,i) = mean(nonzeros(mirgetdata(mirrolloff(miraudio(metalArchives(i)),'frame'))));
       metalrolloffStd(1,i) = std(nonzeros(mirgetdata(mirrolloff(miraudio(metalArchives(i)),'frame'))));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(metalArchives)
       metalfluxMean(1,i) = mean(mirgetdata(mirflux(miraudio(metalArchives(i)),'frame')));
       metalfluxStd(1,i) = std(mirgetdata(mirflux(miraudio(metalArchives(i)),'frame')));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(metalArchives)
       metalzerocrossMean(1,i) = mean(mirgetdata(mirzerocross(miraudio(metalArchives(i)),'frame')));
       metalzerocrossStd(1,i) = std(mirgetdata(mirzerocross(miraudio(metalArchives(i)),'frame')));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(metalArchives)
       metallowenergy(1,i) = mirgetdata(mirlowenergy(miraudio(metalArchives(i))));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(popArchives)
       popCentroidMean(1,i) = mean(mean(mirgetdata(mirtonalcentroid(miraudio(popArchives(i)),'frame'))));
       popCentroidStd(1,i) = std(std(mirgetdata(mirtonalcentroid(miraudio(popArchives(i)),'frame'))));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(popArchives)
       poprolloffMean(1,i) = mean(nonzeros(mirgetdata(mirrolloff(miraudio(popArchives(i)),'frame'))));
       poprolloffStd(1,i) = std(nonzeros(mirgetdata(mirrolloff(miraudio(popArchives(i)),'frame'))));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(popArchives)
       popfluxMean(1,i) = mean(mirgetdata(mirflux(miraudio(popArchives(i)),'frame')));
       popfluxStd(1,i) = std(mirgetdata(mirflux(miraudio(popArchives(i)),'frame')));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(popArchives)
       popzerocrossMean(1,i) = mean(mirgetdata(mirzerocross(miraudio(popArchives(i)),'frame')));
       popzerocrossStd(1,i) = std(mirgetdata(mirzerocross(miraudio(popArchives(i)),'frame')));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(popArchives)
       poplowenergy(1,i) = mirgetdata(mirlowenergy(miraudio(popArchives(i)),'frame'));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(classicalArchives)
       classicalCentroidMean(1,i) = mean(mean(mirgetdata(mirtonalcentroid(miraudio(classicalArchives(i)),'frame'))));
       classicalCentroidStd(1,i) = std(std(mirgetdata(mirtonalcentroid(miraudio(classicalArchives(i)),'frame'))));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(classicalArchives)
       classicalrolloffMean(1,i) = mean(nonzeros(mirgetdata(mirrolloff(miraudio(classicalArchives(i)),'frame'))));
       classicalrolloffStd(1,i) = std(nonzeros(mirgetdata(mirrolloff(miraudio(classicalArchives(i)),'frame'))));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(classicalArchives)
       classicalfluxMean(1,i) = mean(mirgetdata(mirflux(miraudio(classicalArchives(i)),'frame')));
       classicalfluxStd(1,i) = std(mirgetdata(mirflux(miraudio(classicalArchives(i)),'frame')));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(classicalArchives)
       classicalzerocrossMean(1,i) = mean(mirgetdata(mirzerocross(miraudio(classicalArchives(i)),'frame')));
       classicalzerocrossStd(1,i) = std(mirgetdata(mirzerocross(miraudio(classicalArchives(i)),'frame')));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(classicalArchives)
       classicallowenergy(1,i) = mirgetdata(mirlowenergy(miraudio(classicalArchives(i))));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(rockArchives)
       rockCentroidMean(1,i) = mean(mean(mirgetdata(mirtonalcentroid(miraudio(rockArchives(i)),'frame'))));
       rockCentroidStd(1,i) = std(std(mirgetdata(mirtonalcentroid(miraudio(rockArchives(i)),'frame'))));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(rockArchives)
       rockrolloffMean(1,i) = mean(nonzeros(mirgetdata(mirrolloff(miraudio(rockArchives(i)),'frame'))));
       rockrolloffStd(1,i) = std(nonzeros(mirgetdata(mirrolloff(miraudio(rockArchives(i)),'frame'))));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(rockArchives)
       rockfluxMean(1,i) = mean(mirgetdata(mirflux(miraudio(rockArchives(i)),'frame')));
       rockfluxStd(1,i) = std(mirgetdata(mirflux(miraudio(rockArchives(i)),'frame')));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(rockArchives)
       rockzerocrossMean(1,i) = mean(mirgetdata(mirzerocross(miraudio(rockArchives(i)),'frame')));
       rockzerocrossStd(1,i) = std(mirgetdata(mirzerocross(miraudio(rockArchives(i)),'frame')));
     end 
-    for i = 3 : 1 : 10
+    for i = 3 : 1 : len(rockArchives)
       rocklowenergy(1,i) = mirgetdata(mirlowenergy(miraudio(rockArchives(i))));
     end 
     %Eliminate NaN's from rolloff
